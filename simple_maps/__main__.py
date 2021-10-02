3dabd04da12d2c4e1fa4e0d7f70cd0b2dbaa2fce
Julio Batista Silva <julio@juliobs.com>
2021-10-02 12:56:30 -0300

    Update __main__.py: add map get command

--- simple_maps/__main__.py
+++ simple_maps/__main__.py
@@ -1,5 +1,6 @@
 """Tool to create maps with markers using cartes.io API."""
 import typer
+from requests.exceptions import HTTPError
 
 from cartes import Cartes
 
@@ -12,6 +13,19 @@ marker_app = typer.Typer()
 app.add_typer(marker_app, name="marker")
 
 api = Cartes()
+
+
+@map_app.command("get")
+def map_get(map_id: str = typer.Option(..., help="Id of the map")):
+    """Get a single map."""
+    try:
+        response = api.map_get(map_id)
+        typer.echo(response)
+    except HTTPError:
+        typer.secho(
+            f'Error getting map "{map_id}"', fg=typer.colors.RED, err=True
+        )
+        raise typer.Exit(code=1)
 
 
 if __name__ == "__main__":  # pragma: no cover
